e526785713ec37331f5901655e2db942648dc6f6
nur21horin <nurm98371@gmail.com>
2025-04-30 20:31:18 +0600

    first commit

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import Link from "next/link"; 
+import styles from "./navbar.module.css";
+
+const Navbar=()=>{
+    return(
+        <header>
+            <nav className={styles.nav}>
+                <p>Next.js</p>
+                <ul className={styles.links}>
+                <Link href='/'>
+                <li>Home</li>
+                </Link>
+                <Link href='/about'>
+                    <li>About</li>
+                </Link>
+
+                <Link href='/contact'>
+                <li>Contact</li>
+                </Link>
+
+                </ul>
+                
+            </nav>
+        </header>
+    );
+};
+
+export default Navbar;
